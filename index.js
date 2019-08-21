b7854225233680481a232d47a2cb0cb686a3473c
Thang <niken2202@gmail.com>
2019-08-22 00:34:34 +0700

    Update

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const epxress = require('express');
-const { sendMessage,getDTSG ,checkFriends,sendAudio} = require('./FBClient.js');
-const { getWeather} = require('./yahooweather.js');
+const { sendMessage, getDTSG, checkFriends, sendAudio } = require('./FBClient.js');
+const { getWeather } = require('./yahooweather.js');
 const { ids, getMessage } = require('./friends.js');
 const app = epxress();
-
+var timeStr = '23:30';
+var date = new Date();
 async function run() {
-   await getDTSG(function(data){
-    console.log(data);
-});
+    await getDTSG(function (data) {
+        console.log(data);
+    });
     for (var i = 0; i < ids.length; i++) {
         var mess = await getMessage();
-        await sendMessage(ids[i],mess);
+        await sendMessage(ids[i], mess);
         await sleep(5000);
     }
 }
 
-async function runSche(){
-    var date = new Date();
-var current_hour = date.getHours();
+async function runSche() {
+    while (true) {
+        var now = date.getHours() + ':' + date.getMinutes();
+        if (now == timeStr) {
+            await run();
+        }
+        await sleep(30 * 1000);
+    }
+
 }
 function sleep(ms) {
     return new Promise(resolve => {
@@ -25,15 +32,15 @@ function sleep(ms) {
     })
 }
 
-async function test(){
-    await getDTSG(function(data){
+async function test() {
+    await getDTSG(function (data) {
         console.log(data);
     });
     var mess = await getMessage();
-    await sendMessage('100030379368683',mess);
+    await sendMessage('100030379368683', mess);
 }
-async function check(){
-    await getDTSG(function(data){
+async function check() {
+    await getDTSG(function (data) {
         console.log(data);
     });
     for (var i = 0; i < ids.length; i++) {
@@ -41,14 +48,15 @@ async function check(){
         await sleep(3000);
     }
     console.log("done");
-    
+
 }
+runSche();
 app.get('/time/', (req, res) => {
     var date = new Date();
     var current_hour = date.getHours();
-    res.send("date"+date.getHours());
+    res.send(date.getHours() + ':' + date.getMinutes());
 });
- app.get('/run/', (req, res) => {
+app.get('/run/', (req, res) => {
     run();
     res.send("Done!");
 });
@@ -56,25 +64,25 @@ app.get('/', (req, res) => {
     res.send("Created by Thang with 💖 v2.1.2");
 });
 app.get('/dtsg', (req, res) => {
-    getDTSG(function(next){
-    res.send(`dtsg : ${next}`);
+    getDTSG(function (next) {
+        res.send(`dtsg : ${next}`);
     });
-    
+
 });
 app.get('/weather', (req, res) => {
-    getWeather(function(data){
+    getWeather(function (data) {
         res.send(` ${data}`);
     });
-    });
- app.get('/test/', (req, res) => {
+});
+app.get('/test/', (req, res) => {
     test();
     res.send("OK");
-    });
-    app.get('/check/', (req, res) => {
-        check();
-        });  
-      
-    
+});
+app.get('/check/', (req, res) => {
+    check();
+});
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
